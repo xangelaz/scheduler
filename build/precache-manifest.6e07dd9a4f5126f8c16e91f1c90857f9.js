5eb625f8dd401e0e97ed879a7bc40ec69df1e928
Angela Zhou <a03zhou@gmail.com>
2023-11-06 23:33:07 -0500

    add initial Application tests

---
+++ build/precache-manifest.6e07dd9a4f5126f8c16e91f1c90857f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1438b888c91bc17518e1336861f31bb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84497595fa369e1fc67c",
+    "url": "/static/css/2.17e61147.chunk.css"
+  },
+  {
+    "revision": "a5067308b150b803374e",
+    "url": "/static/css/main.15c4832a.chunk.css"
+  },
+  {
+    "revision": "84497595fa369e1fc67c",
+    "url": "/static/js/2.f27e0975.chunk.js"
+  },
+  {
+    "revision": "c91cbb924d2e6dd2f7d23f894d2489e8",
+    "url": "/static/js/2.f27e0975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5067308b150b803374e",
+    "url": "/static/js/main.9b2780c9.chunk.js"
+  },
+  {
+    "revision": "243b8f0ab90e65c7feeb",
+    "url": "/static/js/runtime-main.89e9d1db.js"
+  }
+]);
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,7 +7,7 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 /*
   We import the component that we are testing
@@ -16,14 +16,63 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-/*
-  A test that renders a React Component
-*/
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Form", () => {
 
-  return waitForElement(() => getByText("Monday")).then(() => {
+  const interviewers = [
+    {
+      id: 1,
+      student: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  /*
+    A test that renders a React Component
+  */
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
+  /*
+  An alternative way to write the above test is:
+
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+
     fireEvent.click(getByText("Tuesday"));
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+  */
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    // console.log(prettyDOM(container));
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment))
+  })
+
 });
